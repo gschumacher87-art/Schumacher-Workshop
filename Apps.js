04284729f4fad44bef186ad4a9a2ee3787626de9
gschumacher87-art <gschumacher87@gmail.com>
2026-03-20 13:33:14 +1100

    Update Apps.js

--- Apps.js
+++ Apps.js
@@ -1,1 +1,10 @@
+function updateClock() {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString();
+    document.getElementById('liveClock').textContent = timeString;
 
+    const dateString = now.toLocaleDateString();
+    document.getElementById('currentDate').textContent = dateString;
+}
+setInterval(updateClock, 1000);
+updateClock();
